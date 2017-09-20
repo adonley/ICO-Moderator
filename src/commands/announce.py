894f054e610e856d0ffc0eaf93f39b210543bdf7
Andrew Donley <andrewjdonley@gmail.com>
2017-09-20 04:07:10 -0700

    only post if someone has spoken

--- src/commands/announce.py
+++ src/commands/announce.py
@@ -1,4 +1,5 @@
 import asyncio
+import discord
 import util
 
 
@@ -7,6 +8,10 @@ class Timer(util.Listener):
     def __init__(self):
         super().__init__()
         self._timeout = 25.0 * 60.0
+        self._announce_channels = ["unikoingold", "random", "crypto-security"]
+        self._should_post = dict()
+        for chan in self._announce_channels:
+            self._should_post[chan] = True
 
     async def on_start(self):
         await self._job()
@@ -16,10 +21,16 @@ class Timer(util.Listener):
         message += "direct message you with an address to send funds or ask you to use any website. Please report any attempt to "
         message += "do so to a moderator. Thank you."
         for channel in self.client.get_all_channels():
-            if channel.name == "unikoingold" or channel.name == "random" or channel.name == "crypto-security":
-                await self.client.send_message(channel, message)
+            if channel.name in self._announce_channels:
+                if self._should_post[channel.name]:
+                    self._should_post[channel.name] = False
+                    await self.client.send_message(channel, message)
         await asyncio.sleep(self._timeout)
         await self._job()
+
+    async def on_message(self, msg: discord.Message):
+        if msg.channel in self._announce_channels:
+            self._should_post[msg.channel] = True
 
     def cancel(self):
         self._task.cancel()
